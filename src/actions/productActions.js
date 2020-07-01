ddbf2933a4c5c2dfae87e1004a9ed77adb401020
AlanAlv <alan.alv96@gmail.com>
2020-06-30 22:05:20 -0500

    Add products to API

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,15 +3,22 @@ import {
     ADD_PRODUCT_SUCCESS,
     ADD_PRODUCT_ERROR
 } from '../types'
+import clientAxios from '../config/axios';
 
 // Create New Products
 export function createNewProductAction(product){
-    return (dispatch) => {
+    return async  (dispatch) => {
         dispatch(addProduct());
 
         try {
+            // Insert to API
+            await clientAxios.post('/products', product);
+
+            // Updates state if inserted successfuly
             dispatch(addProductSuccess(product));
         } catch (error) {
+            console.log(error);
+            // Updates state if insert fails
             dispatch(addProductError(true));
         }
     }
