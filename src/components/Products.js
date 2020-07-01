1530e036a648bfdd5bf8ae48d063f667972c4cb1
AlanAlv <alan.alv96@gmail.com>
2020-07-01 00:58:38 -0500

    Get product error & loading

--- src/components/Products.js
+++ src/components/Products.js
@@ -17,13 +17,37 @@ const Products = () => {
     }, [])
 
     // Get state
-    const products = useSelector(state => state.products.products)
+    const products = useSelector(state => state.products.products);
+    const error = useSelector(state => state.products.error);
+    const loading = useSelector(state => state.products.loading);
+
 
     return ( 
         <Fragment>
             <h2 className="text-center my-5">
                 Products List
             </h2>
+
+            {
+                error   
+                    ?
+                        <p className="font-weight-bold alert alert-danger text-center mt4">
+                            There was an error
+                        </p>
+                    :
+                        null
+            }
+
+            {
+                loading   
+                    ?
+                        <p className="text-center mt4">
+                            Loading...
+                        </p>
+                    :
+                        null
+            }
+
 
             <table className="table table-striped">
                 <thead className="bg-primary table-dark">
@@ -36,7 +60,7 @@ const Products = () => {
 
                 <tbody>
                     {
-                        products.lenth === 0 
+                        products.length === 0 
                             ?
                                 'There are no products'
                             :
